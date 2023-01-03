af08bd201d5fafbfc46e5b63238d613d6a41a76c
Irving Saenz Colon <irving.saenz.colon@gmail.com>
2023-01-02 19:05:36 -0600

    fixed a little bug when last element is deleted from cart

--- js/main.js
+++ js/main.js
@@ -350,11 +350,15 @@ function onRemoveFromCart(event){
         
         return;
     }
-    setTimeout(function(){
-        article.remove();
-        cart_container.classList.add('cart-container--empty');
-        cart_container.appendChild(emptyCartHTML());
-    }, 500);
+    else{
+        if(cart.length)
+            return;
+        setTimeout(function(){
+            article.remove();
+            cart_container.classList.add('cart-container--empty');
+            cart_container.appendChild(emptyCartHTML());
+        }, 500);
+    }
     
 
 }
@@ -480,14 +484,16 @@ function productHTML(product){
     product_button.classList.add('product__button');
     product_button.addEventListener('click', onAddToCart);
 
-    const cart_svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
-    cart_svg.setAttribute('viewBox', '0 0 576 512');
+    const cart_img = document.createElement('img');
+    cart_img.setAttribute('src', './assets/icons/cart_white.svg');
+    // const cart_svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+    // cart_svg.setAttribute('viewBox', '0 0 576 512');
 
-    const cart_path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    cart_path.setAttribute('d', 'M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z');
+    // const cart_path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    // cart_path.setAttribute('d', 'M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z');
     
-    cart_svg.appendChild(cart_path);
-    product_button.appendChild(cart_svg);
+    // cart_svg.appendChild(cart_path);
+    product_button.appendChild(cart_img);
 
     product_info.append(product_name, product_price);
 
